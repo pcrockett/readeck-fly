c1d7ca07892e6b43e7f10dedf8375ebf631b72b5
Phil Crockett <contact@philcrockett.com>
2025-07-19 21:27:21 +0200

    backup script

---
+++ bin/backup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+init() {
+    BACKUP_DIR="backup"
+    NOW_UTC="$(date -Iminutes -u)"  # cryptic flags for busybox compatibility
+    BACKUP_ARCHIVE="${BACKUP_DIR}/${NOW_UTC}.zip"
+
+    step "Waking up server..."
+    curl "${READECK_ROOT_URL}" &> /dev/null
+}
+
+step() {
+    echo "--> $*"
+}
+
+main() {
+    init
+
+    local backup_commands="
+mkdir -p '${BACKUP_DIR}'
+readeck export -config config.toml '${BACKUP_ARCHIVE}'
+exit
+"
+    step "Running export..."
+    echo "${backup_commands}" | flyctl ssh console
+
+    step "Downloading archive..."
+    flyctl ssh sftp get "${BACKUP_ARCHIVE}"
+
+    mkdir -p backup
+    mv "${NOW_UTC}.zip" backup
+
+    step "Cleaning up..."
+    flyctl ssh console --command "rm -r '${BACKUP_DIR}'"
+
+    exit $?
+}
+
+main
